da2ae92e0167b484609becb56f75630d5568a06a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-25 11:38:50 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,10 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
+
+mkdir -p ./db
+mkdir -p ./elastic
+mkdir -p ./files
+
+chown -R 1000:1000 ./db
+chown -R 1000:1000 ./elastic
+chown -R 1000:1000 ./files
